b56e6a8d11416e9a941841f4b6c739a4204b33ec
김산해 <kimsanhae@gimsanhaes-MacBook-Air.local>
2022-03-13 18:55:03 +0900

    Add UsersList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import AddUser from './Users/AddUser';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} /> {/* set users as a empty array */}
     </div>
   );
 }
---
+++ src/components/UI/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+
+import classes from './Button.module.css';
+
