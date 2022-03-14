2fa7be91dc2897a83abc42cae91488195110ff16
김산해 <kimsanhae@gimsanhaes-MacBook-Air.local>
2022-03-14 21:42:05 +0900

    add error modal

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ErrorModal = props => {
+    return (
+        <div>
+<header>
+    <h2>{props.title}</h2>
+</header>
+<div>
+    <p>{props.message}</p>
+</div>
+<footer>
+    <button>ok</button>
+</footer>
+</div>
+    )
+};
+
+export default ErrorModal;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,29 @@
 import React, {useState} from 'react'
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState(''); 
 
-    const addUserHandler = (event) => {
+    const [error, setError] = useState();
+
+        const addUserHandler = (event) => {
         event.preventDefault();
         
         if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+               title: 'Invalid input',
+               message: 'Please enter a valid name and age(non-empty values).' 
+            });
+            
             return;
         }
         if (+enteredAge < 1) { //ENTEREDAGE IS STRING, NOT INTEGER (TO BE SECURE, ADD '+' SO NEGATIVE NUMBER DOES NOT GO THROUGH THE VALIDATION)
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter an interger bigger than 0.' 
+             });
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
@@ -27,7 +39,13 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value)
     };
 
+    const errorHandler = ()=> {
+        setError(null);
+    };
+
     return (
+    <div>
+        {error && <ErrorModal title={error.title} message={error.message} />}
     <Card>
     <form onSubmit={addUserHandler}>
         <label htmlFor="username">User Name</label>
@@ -37,6 +55,7 @@ const AddUser = (props) => {
         <button type="submit">Add User</button>
     </form>
     </Card>
+    </div>
     )
 };
 
